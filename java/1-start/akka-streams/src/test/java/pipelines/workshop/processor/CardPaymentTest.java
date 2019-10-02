e40045ff86d1638046c937031baa486897d83ee9
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2019-10-02 11:58:11 +0200

    Fixed Json test.

--- java/1-start/akka-streams/src/test/java/pipelines/workshop/processor/CardPaymentTest.java
+++ java/1-start/akka-streams/src/test/java/pipelines/workshop/processor/CardPaymentTest.java
@@ -7,7 +7,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.util.ByteString;
 import junit.framework.TestCase;
-import pipelines.workshop.schema.java.CardPayment;
+import pipelines.workshop.ingress.CardPaymentJson;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -18,12 +18,12 @@ public class CardPaymentTest extends TestCase {
         Materializer mat = ActorMaterializer.create(system);
 
         String jsonStr = "{\"timestamp\":1538006400000,\"countryCode\":\"KR\",\"customerId\":\"6fb55cb1-3aa6-472b-a99c-306081421ba6\",\"deviceId\":\"d6756993-2e55-4373-8feb-1b65e08828b9\",\"merchantId\":\"76f01f05-6df9-491d-8c82-13cc12d000af\",\"amount\":88.14}";
-        CardPayment expected = new CardPayment(1538006400000L, "KR", "6fb55cb1-3aa6-472b-a99c-306081421ba6", "d6756993-2e55-4373-8feb-1b65e08828b9", "76f01f05-6df9-491d-8c82-13cc12d000af", 88.14);
+        CardPaymentJson expected = new CardPaymentJson(1538006400000L, "KR", "6fb55cb1-3aa6-472b-a99c-306081421ba6", "d6756993-2e55-4373-8feb-1b65e08828b9", "76f01f05-6df9-491d-8c82-13cc12d000af", 88.14);
 
-        Unmarshaller<ByteString, CardPayment> unmarshaller = Jackson.<CardPayment>byteStringUnmarshaller(CardPayment.class);
+        Unmarshaller<ByteString, CardPaymentJson> unmarshaller = Jackson.<CardPaymentJson>byteStringUnmarshaller(CardPaymentJson.class);
         ByteString byteString = ByteString.fromString(jsonStr);
 
-        CompletableFuture<CardPayment> f = (CompletableFuture<CardPayment>) unmarshaller.unmarshal(byteString, mat);
+        CompletableFuture<CardPaymentJson> f = (CompletableFuture<CardPaymentJson>) unmarshaller.unmarshal(byteString, mat);
 
         assertEquals(expected, f.get());
     }
